b3f8121be74e9e19fe9a5f8988d835557c6a9922
Wisdom Iwunwa <Wisdomiwunwa@gmail.com>
2024-07-08 21:52:50 +0100

    FavIcon and Title Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
-
+import { useEffect } from 'react';
 import './App.css';
 import Index from './Pages';
 
+
+
 const App = () => {
+
+  useEffect(() => {
+    document.title = " Wisdom Iwunwa";
+  });
 
   return (
     < div>
