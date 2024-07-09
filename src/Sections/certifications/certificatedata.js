d23cfbdcb2945d53e01d0d1d458b4a45cd21c063
Wisdom Iwunwa <Wisdomiwunwa@gmail.com>
2024-07-09 04:31:09 +0100

    Minor Adjustments

--- src/Sections/certifications/certificatedata.js
+++ src/Sections/certifications/certificatedata.js
@@ -2,22 +2,22 @@ export const certifications = [
     {
         id:1,
         title: 'TECH1M',
-        description: 'A portfolio website that houses the works of an NFT and a digital artist.',
-        // image : 'emmy-walka.png',
+        // description: 'A portfolio website that houses the works of an NFT and a digital artist.',
+        // // image : 'emmy-walka.png',
         // link: 'https://www.emmywalka.com '
     },
     {
         id:2,
         title: 'ZURI X I4G',
-        description: 'A portfolio website that houses the works of an NFT and a digital artist.',
-        // image : 'emmy-walka.png',
+        // description: 'A portfolio website that houses the works of an NFT and a digital artist.',
+        // // image : 'emmy-walka.png',
         // link: 'https://www.emmywalka.com '
     },
     {
         id:3,
         title: 'RANDOM',
-        description: 'A portfolio website that houses the works of an NFT and a digital artist.',
-        // image : 'emmy-walka.png',
+        // description: 'A portfolio website that houses the works of an NFT and a digital artist.',
+        // // image : 'emmy-walka.png',
         // link: 'https://www.emmywalka.com '
     },
 ]
